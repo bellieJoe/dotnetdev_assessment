315a2b59e5d2885ef24d39c779e2e969ea17cecf
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2024-09-06 10:52:22 +0800

    Successfully attached the database on the App_Data

--- Model.cs
+++ Model.cs
@@ -4,16 +4,19 @@ using Microsoft.EntityFrameworkCore;
 public class ApplicationDbContext : DbContext
 {
     private readonly IConfiguration _configuration;
+    private readonly IWebHostEnvironment _environment;
     public DbSet<Employee> Employees { get; set; }
 
-    public ApplicationDbContext(IConfiguration configuration)
+    public ApplicationDbContext(IConfiguration configuration, IWebHostEnvironment environment)
     {
         _configuration = configuration;
+        _environment = environment;
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(_configuration.GetConnectionString("ApplicationDbContext"));
+        optionsBuilder.UseSqlServer("Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=dotnetdev_assessment;Integrated Security=SSPI;AttachDBFilename=" + Path.Combine(_environment.ContentRootPath, "App_Data") + "\\dotnetdev_assessment.mdf;TrustServerCertificate=True");
+        //optionsBuilder.UseSqlServer(_configuration.GetConnectionString("ApplicationDbContext"));
         base.OnConfiguring(optionsBuilder);
     }
 
